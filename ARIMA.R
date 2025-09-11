318a83691d0400c31f48b42071dc124babac311c
Wei He <limweihe10@gmail.com>
2025-09-11 21:41:59 +0800

    rewrite the graph labels

--- ARIMA.R
+++ ARIMA.R
@@ -3,11 +3,10 @@ ARIMA_method <- function(ts_train, ts_test) {
   ndiffs(ts_train)
   nsdiffs(ts_train)
   
-  
   # -------------------------------
   # Manual ARIMA
   # -------------------------------
-  fit <- Arima(ts_train, order=c(0,1,0), seasonal=list(order =c(2,1,0), period=12))
+  fit <- Arima(ts_train, order=c(2,1,0), seasonal=list(order =c(2,1,0), period=12))
   checkresiduals(fit)
   summary(fit)
   
@@ -23,12 +22,14 @@ ARIMA_method <- function(ts_train, ts_test) {
   # -------------------------------
   # Auto ARIMA
   # -------------------------------
-  fit2 <- auto.arima(ts_train, seasonal = T, trace = T, approximation = F)            
+  fit2 <- auto.arima(ts_train, seasonal = T, trace = T, approximation = F, ic="aic")            
   checkresiduals(fit2)
   summary(fit2)
   
   # Forecast for the auto fit arima
   fr <- forecast(fit2, h = length(test))
+  layout(matrix(c(1,1)))
+  plot(fit2)
   layout(matrix(c(1,1)))
   plot(fr)
   lines(ts_test, col="turquoise2")
--- preprocessing.R
+++ preprocessing.R
@@ -1,7 +1,7 @@
 preprocess_data <- function(df) {
   cat("\nBefore preprocessing:", nrow(df), "rows ×", ncol(df), "columns\n")
   
-  # Remove missing values (for Production)
+  # Remove missing values
   missing_count <- sum(is.na(df$average))
   if (missing_count > 0) {
     cat("Missing values detected in dataset:", missing_count, "→ removing rows.\n")
@@ -19,41 +19,40 @@ preprocess_data <- function(df) {
     cat("No duplicate rows detected.\n")
   }
   
-  # Formating the date  
-  df <- df %>%
-    mutate(
-      date_char = as.character(date),
-      previous_date = lag(date_char),
-      month_num = ifelse(
-        grepl("\\.1$", date_char) & grepl("\\.9$", previous_date),
-        10,
-        as.numeric(sub(".*\\.", "", date_char))
-      ),
-      date = ymd(paste(floor(as.numeric(date)), month_num, "01", sep = "-"))
-    ) %>%
-    select(date, average)
+  
+  # Create a date column for the monthly data
+  df$date <- as.Date(paste(df$year, df$month, "01", sep = "-"))
   
   df <- df %>%
-    filter(year(date) >= 2010)
-
+    select(date, average)
+  
+  # Select the recent 14 years
+  df <- df %>%
+    filter(date >= as.Date("2010-01-01"))
+  
   min_date <- min(df$date)  
   min_year <- as.numeric(format(min_date, "%Y"))
   min_month <- as.numeric(format(min_date, "%m"))
   ts_data <- ts(df$average, start=c(min_year, min_month), frequency=12)
   
-  # Now you can use cycle()
+  # Box plot
   boxplot(ts_data ~ cycle(ts_data),
           xlab = "Month",
           ylab = "Avg Concentration",
-          main = "Boxplot of Monthly Nitrous Oxide Concentration")
+          main = "Boxplot of Monthly CO2 Concentration from Jan 2010 to Dec 2023")
+
   
-  
-  # Detect and remove outliers (IQR method for Production)
+  # Calculate the IQR, Q1, and Q3
   Q1 <- quantile(df$average, 0.25, na.rm = TRUE)
   Q3 <- quantile(df$average, 0.75, na.rm = TRUE)
   IQR <- Q3 - Q1
-  outlier_idx <- which(df$average < (Q1 - 1.5 * IQR) | 
-                         df$average > (Q3 + 1.5 * IQR))
+  
+  # Calculate the outlier thresholds
+  lower_bound <- Q1 - 1.5 * IQR
+  upper_bound <- Q3 + 1.5 * IQR
+  
+  outlier_idx <- which(df$average < (lower_bound) | 
+                         df$average > (upper_bound))
   
   if (length(outlier_idx) > 0) {
     cat("Outliers detected in dataset:", length(outlier_idx), "→ removing outliers.\n")
@@ -61,6 +60,7 @@ preprocess_data <- function(df) {
   } else {
     cat("No outliers detected in dataset\n")
   }
+
   
   cat("\nAfter preprocessing:", nrow(df), "rows ×", ncol(df), "columns\n")
   
